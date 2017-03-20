9cf6658d17df6e3dcf70f9cf930c2baabccea0f2
anne55x9 <anne55x9@edu.easj.dk>
2017-03-20 19:35:42 +0100

    Implementeret en get http med LINQ sætninger som kalder hoteller i roskilde med deres værelser anvist (opg3).

--- ClientHotel_webservice/Program.cs
+++ ClientHotel_webservice/Program.cs
@@ -12,7 +12,7 @@ namespace ClientHotel_webservice
     {
         static void Main(string[] args)
         {
-            const string serverUrl = "http://localhost.fiddler:18953";
+            const string serverUrl = "http://localhost.fiddler:18953"; 
 
             //Guest g1 = new Guest();
 
@@ -20,7 +20,6 @@ namespace ClientHotel_webservice
             //g1.Name = "Martin";
 
 
-          
 
             Console.WriteLine("Opgave 1. Get hotel 3.");
 
@@ -57,10 +56,8 @@ namespace ClientHotel_webservice
                 using (var client2 = new HttpClient())
                 {
                     client2.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-
                     client2.BaseAddress = new Uri(serverUrl);
                     client2.DefaultRequestHeaders.Clear();
-
                     string urlString2 = "api/hotels";
 
                     try
@@ -74,10 +71,13 @@ namespace ClientHotel_webservice
                             {
                                 Console.WriteLine("hotel : " + hotel.Name + "hoteladresse : " + hotel.Address);
                             }
-
                             var hotelRoskilde = from h in hotelList
                                                 where h.Address.Contains("Roskilde")
                                                 select h;
+                            Console.WriteLine("Hoteller i Roskilde:");
+
+                            ///Lamda sætning. 
+                            // var hotellistRoskildeRum = response3.Content.ReadAsAsync<List<Hotel>>().Result.Where(x => x.Address.Contains("Roskilde");
 
 
                             foreach (var item in hotelRoskilde)
@@ -85,9 +85,49 @@ namespace ClientHotel_webservice
                                 Console.WriteLine("hotel : " + item.Name + "hoteladresse : " + item.Address);
                             }
 
-
                         }
+                    }
+                    catch (Exception e)
+                    {
 
+                        Console.WriteLine("Der er sket en fejl : " + e.Message);
+                    }
+                }
+
+                Console.WriteLine("Opgave 3, Get all hotels in roskilde and their rooms:");
+
+                using (var client3 = new HttpClient())
+                {
+                    client3.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                    client3.BaseAddress = new Uri(serverUrl);
+                    client3.DefaultRequestHeaders.Clear();
+                    string urlString3 = "api/hotels";
+
+                    try
+                    {
+                        HttpResponseMessage response3 = client3.GetAsync(urlString3).Result;
+                        if (response3.IsSuccessStatusCode)
+                        {
+                            var hotelList = response3.Content.ReadAsAsync<List<Hotel>>().Result;
+
+                            Console.WriteLine("Alle hoteller:");
+
+                            foreach (var hotel in hotelList)
+                            {
+                                Console.WriteLine("hotel : " + hotel.Name + "hoteladresse : " + hotel.Address);
+                            }
+
+                            var hotelRoskilde = from h in hotelList
+                                                where h.Address.Contains("Roskilde")
+                                                select h;
+
+                            Console.WriteLine("Hoteller i Roskilde:");
+
+                            foreach (var item in hotelRoskilde)
+                            {
+                                Console.WriteLine("hotel : " + item.Name + "hoteladresse: " + item.Address);
+                            }
+                        }
                     }
                     catch (Exception e)
                     {
@@ -95,9 +135,96 @@ namespace ClientHotel_webservice
                         Console.WriteLine("Der er sket en fejl : " + e.Message);
                     }
 
-                 
+                }
+
+                Console.WriteLine("Opgave 3 Fortsat:");
+
+                
+
+                using (var client4 = new HttpClient())
+                {
+                    client4.BaseAddress = new Uri(serverUrl);
+                    client4.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                    client4.DefaultRequestHeaders.Clear();
+                    string urlString4 = "api/rooms";
+                    string urlString5 = "api/hotels";
+
+                    
+
+
+                    try
+                    {
+                        
+                        HttpResponseMessage response4 = client4.GetAsync(urlString4).Result;
+                        HttpResponseMessage response5 = client4.GetAsync(urlString5).Result; 
+                        if (response4.IsSuccessStatusCode && response5.IsSuccessStatusCode)
+                        {
+                           
+
+                            Console.WriteLine("alle rum:");
+
+                            var rumliste = response4.Content.ReadAsAsync<List<Room>>().Result;
+
+                           
+
+                            var hotelliste = response5.Content.ReadAsAsync<List<Hotel>>().Result;
+
+                            var hotelRoskilde = from h in hotelliste
+                                                where h.Address.Contains("Roskilde")
+                                                select h;
+
+                            foreach (var item in hotelRoskilde)
+                            {
+                                Console.WriteLine($"{item.Address} {item.Name}");
+                            }
+
+                            //var rumliste2  = 
+                            //    from r in rumliste
+                            //    join h in hotelliste
+                            //    on r.Hotel_No equals h.Hotel_No
+
+                            var rumlisteRoskilde =
+                             from h in hotelRoskilde
+                             join r in rumliste
+                             on h.Hotel_No equals r.Hotel_No
+                             where h.Address.Contains("Roskilde")
+                             select new
+                             {
+                                 r.Types,
+                                 h.Name,
+                                 h.Address
+                             };
+
+                            foreach (var item in rumlisteRoskilde)
+                            {
+                                Console.WriteLine(item);
+                            }
+                                
+
+                               
+
+                            //foreach (var item in rumliste)
+                            //{
+                            //    Console.WriteLine($"Rum type: {item.Types} , Pris: {item.Price}, Nummer {item.Room_No}");
+                            //}
+
+
+
+
+                        }
+
+
+
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("Der er sket en fejl : " + e.Message);
+
+                    }
+                    
 
                 }
+
 
                 Console.ReadLine();
             }
