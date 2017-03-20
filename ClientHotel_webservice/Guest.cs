2258ebf9119591ea637dd7cde4e2a83f2fa75b59
anne55x9 <anne55x9@edu.easj.dk>
2017-03-20 13:42:13 +0100

    Oprettet Guest klasse samt using Net.http med htttpClient.

---
+++ ClientHotel_webservice/Guest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientHotel_webservice
+{
+    class Guest
+    {
+
+        public int Guest_No { get; set; }
+
+        public string Name { get; set; }
+
+        public string Address { get; set; }
+    }
+}
--- ClientHotel_webservice/Program.cs
+++ ClientHotel_webservice/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,16 @@ namespace ClientHotel_webservice
     {
         static void Main(string[] args)
         {
+            Guest g1 = new Guest();
+
+            g1.Guest_No = 200;
+            g1.Name = "Martin";
+
+            using (var client = new HttpClient())
+            {
+
+            }
+
         }
     }
 }
