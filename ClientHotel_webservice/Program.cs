b30d0817ba2bdf27b077e152247c9114c62250fe
anne55x9 <anne55x9@edu.easj.dk>
2017-03-20 14:40:27 +0100

    Implementeret Get på alle hoteller og hoteller kun i roskilde via LINQ

--- ClientHotel_webservice/Program.cs
+++ ClientHotel_webservice/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,14 +12,94 @@ namespace ClientHotel_webservice
     {
         static void Main(string[] args)
         {
-            Guest g1 = new Guest();
+            const string serverUrl = "http://localhost.fiddler:18953";
 
-            g1.Guest_No = 200;
-            g1.Name = "Martin";
+            //Guest g1 = new Guest();
+
+            //g1.Guest_No = 200;
+            //g1.Name = "Martin";
+
+
+          
+
+            Console.WriteLine("Opgave 1. Get hotel 3.");
 
             using (var client = new HttpClient())
             {
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
 
+                client.BaseAddress = new Uri(serverUrl);
+                client.DefaultRequestHeaders.Clear();
+                
+                string urlString = "api/hotels/3";
+                try
+                {
+                    HttpResponseMessage response = client.GetAsync(urlString).Result;
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var hotel3 = response.Content.ReadAsAsync<Hotel>().Result;
+                        Console.WriteLine("hotel : " + hotel3.Name + "hoteladresse : " + hotel3.Address);
+
+                        //foreach (var hotel in hotelList)
+                        //{
+                        //    Console.WriteLine("hotel : " + hotel.Name + "hoteladresse : " + hotel.Address);
+                        //}
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Der er sket en fejl : " + e.Message);
+                }
+
+                Console.WriteLine("Opgave 2. Get alle hoteller:");
+
+
+                using (var client2 = new HttpClient())
+                {
+                    client2.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                    client2.BaseAddress = new Uri(serverUrl);
+                    client2.DefaultRequestHeaders.Clear();
+
+                    string urlString2 = "api/hotels";
+
+                    try
+                    {
+                        HttpResponseMessage response2 = client2.GetAsync(urlString2).Result;
+                        if (response2.IsSuccessStatusCode)
+                        {
+                            var hotelList = response2.Content.ReadAsAsync < List < Hotel>>().Result;
+
+                            foreach (var hotel in hotelList)
+                            {
+                                Console.WriteLine("hotel : " + hotel.Name + "hoteladresse : " + hotel.Address);
+                            }
+
+                            var hotelRoskilde = from h in hotelList
+                                                where h.Address.Contains("Roskilde")
+                                                select h;
+
+
+                            foreach (var item in hotelRoskilde)
+                            {
+                                Console.WriteLine("hotel : " + item.Name + "hoteladresse : " + item.Address);
+                            }
+
+
+                        }
+
+                    }
+                    catch (Exception e)
+                    {
+
+                        Console.WriteLine("Der er sket en fejl : " + e.Message);
+                    }
+
+                 
+
+                }
+
+                Console.ReadLine();
             }
 
         }
--- ClientHotel_webservice/Room.cs
+++ ClientHotel_webservice/Room.cs
@@ -8,23 +8,17 @@ namespace ClientHotel_webservice
 {
     public class Room
     {
-
-     
         public int Room_No { get; set; }
 
-       
         public int Hotel_No { get; set; }
-
      
         public string Types { get; set; }
 
         public double? Price { get; set; }
-
         
         public virtual ICollection<Booking> Booking { get; set; }
 
         public virtual Hotel Hotel { get; set; }
-
         
         //public virtual ICollection<Facilities> Facilities { get; set; }
 
